6bf925494f80494b07ebcdb462ef976cd3e13fec
你的名字 <>
2024-04-15 12:00:33 +0800

    add count

--- r.py
+++ r.py
@@ -1,13 +1,15 @@
 import random
 r = random.randint(1, 100) #.=的 產生隨機數功能1~100
-
+count = 0
 while True:
+	count += 1 #count = count + 1
 	num = input('請猜一個1-100之間的數字: ')
 	num = int(num)
 	if num == r:
-		print('猜中了!')
+		print('猜中了!這是你猜的第', count , '次')
 		break
 	elif num > r:
 		print('比答案大')
 	else:
 		print('比答案小')
+	print('這是你猜的第', count , '次') #在if架構之外，才不用重複寫
